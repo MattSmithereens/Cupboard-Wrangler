bfda277f62ed3606a54c96b27b87764b5731cac8
Matt Smith <mattsmithereens@gmail.com>
2019-03-07 22:57:01 -0800

    commented non-functioal dispatch

--- src/components/ItemListItem.js
+++ src/components/ItemListItem.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux'
-import { toggleListItem } from '../actions/items';
+import { toggleListItem } from '../actions/items'; //doesn't work yet
 
 const ItemListItem = (props) => (
   <li>
@@ -22,12 +22,13 @@ const ItemListItem = (props) => (
   </li>
 );
 
-const mapStateToProps = (state, props) => {
-  return {
-    item: state.items.find((item) => {
-      return item.id === props.match.params.id;
-    })
-  }
-};
+// legit don't know how to code this part
+// const mapStateToProps = (state, props) => {
+//   return {
+//     item: state.items.find((item) => {
+//       return item.id === props.match.params.id;
+//     })
+//   }
+// };
 
 export default connect()(ItemListItem)
