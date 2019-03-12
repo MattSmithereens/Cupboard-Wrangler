2570063b507d43678c741ce665993f84a5686fd9
Matt Smith <mattsmithereens@gmail.com>
2019-03-11 19:18:58 -0700

    minor styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,15 @@ import { NavLink } from 'react-router-dom';
 
 const Header = () => (
   <header>
-    <div className='title'><h3>Cupboard Wrangler!</h3></div>
-    <div className='options'>
-      <p>
-        <NavLink to='/Create' activeClassName='isActive'>Add Item</NavLink> |
+    <div className='flex-box-container'>
+      <div className='title container'><h3>Cupboard Wrangler!</h3></div>
+      <div className='options container'>
+        <p>
+          <NavLink to='/Create' activeClassName='isActive'>Add Item</NavLink> |
         <NavLink to='/' activeClassName='isActive' exact={true}>Shopping List</NavLink> |
         <NavLink to='/Help' activeClassName='isActive'>Help</NavLink>
-      </p>
+        </p>
+      </div>
     </div>
   </header>
 )
