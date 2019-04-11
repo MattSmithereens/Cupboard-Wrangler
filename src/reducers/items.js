da7dbb1325abaf52e4d6a922fa72c309ae7b1091
Matt Smith <mattsmithereens@gmail.com>
2019-04-11 15:21:20 -0700

    fix toggle date again

--- src/reducers/items.js
+++ src/reducers/items.js
@@ -1,3 +1,5 @@
+import moment from 'moment';
+
 const itemsReducerDefaultState = [];
 
 export default (state = itemsReducerDefaultState, action) => {
@@ -23,7 +25,12 @@ export default (state = itemsReducerDefaultState, action) => {
     case 'TOGGLE_LIST_ITEM':
       return state.map(item =>
         (item.id === action.id)
-          ? { ...item, inCupboard: !item.inCupboard }
+          ? {
+            ...item,
+            inCupboard: !item.inCupboard,
+            // reset shelfLife when item is toggled
+            shelfLife: new moment().add(item.amount, 'day').format('L')
+          }
           : item
       )
     case 'SET_ITEMS':
