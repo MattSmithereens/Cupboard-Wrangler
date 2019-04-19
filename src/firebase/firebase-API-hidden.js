3cb94c0919d6d0e0a0143858b179193371361af8
Matt Smith <mattsmithereens@gmail.com>
2019-04-19 13:53:43 -0700

    archived firebase w/obscured API

---
+++ src/firebase/firebase-API-hidden.js
@@ -0,0 +1,52 @@
+import * as firebase from 'firebase';
+import API_KEY from '../config';
+
+const config = {
+  apiKey: API_KEY,
+  authDomain: "cupboard-wrangler.firebaseapp.com",
+  databaseURL: "https://cupboard-wrangler.firebaseio.com",
+  projectId: "cupboard-wrangler",
+  storageBucket: "cupboard-wrangler.appspot.com",
+  messagingSenderId: "315154339220"
+};
+
+firebase.initializeApp(config);
+
+const database = firebase.database();
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+
+export { firebase, googleAuthProvider, database as default };
+
+
+// database.ref()
+//   .once('value')    // use .on() to subscribe
+//   .then((snapshot) => {
+//     const val = snapshot.val();
+//     console.log(val);
+//   })
+//   .catch((e) => {
+//     console.log('error', e);
+//   });
+
+// database.ref().set({                  // set (ie, write) values to DB
+//   name: 'Matt',
+//   age: 44,
+//   isBro: true,
+//   location: {
+//     city: 'portland',
+//     country: 'USA'
+//   },
+//   anotherThing: 'yes',
+// }).then(() => {                         // promise
+//   console.log('data saved!')
+// }).catch((e) => {
+//   console.log('this failed', e);
+// });
+
+// database.ref('anotherThing')
+//   .remove()
+//   .then(() => {
+//     console.log('item deleted!');
+//   }).catch((e) => {
+//     console.log('nope', e);
+//   });
