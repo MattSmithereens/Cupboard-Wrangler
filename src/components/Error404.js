f6601b2e0fa98000f812988048fe8c33b2d5a2b1
Matt Smith <mattsmithereens@gmail.com>
2019-04-19 16:25:56 -0700

    add authentication + private routes

--- src/components/Error404.js
+++ src/components/Error404.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Error404 = () => (
   <div className='top-buffer'>
     <p>This page doesn't appear to exist</p>
-    <p><Link to='/'>go home</Link></p>
+    <p><Link to='/Dashboard'>go home</Link></p>
   </div>
 );
 
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,33 +1,33 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Header from '../components/Header.js';
+import { Router, Route, Switch } from 'react-router-dom';
+import createHistory from 'history/createBrowserHistory';
 import EntryList from '../components/EntryList.js';
 import CreateItem from '../components/CreateItem.js';
 import EditItem from '../components/EditItem.js';
 import Help from '../components/Help.js';
 import Error404 from '../components/Error404.js';
 import CupboardItemList from '../components/CupboardItemList.js';
-import LoginPage from '../components/LoginPage.js'
+import LoginPage from '../components/LoginPage.js';
+import PrivateRoute from './PrivateRoute';
+
+export const history = createHistory();
 
 const AppRouter = () => (
-  <BrowserRouter>
-    <div>
-      <Header />
-      <div className='container'>
-        <Switch>
-          <Route path='/' component={LoginPage} exact={true} />
-          <Route path='/Dashboard' component={EntryList} />
-          <Route path='/Cupboard' component={CupboardItemList} />
-          <Route path='/Create' component={CreateItem} />
-          <Route path='/Edit/:id' component={EditItem} />
-          <Route path='/Help' component={Help} />
-          <Route path='/About' component={Help} />
-          <Route path='/Login' component={LoginPage} />
-          <Route component={Error404} />
-        </Switch>
-      </div>
+  <Router history={history}>
+    <div className='container'>
+      <Switch>
+        <Route path='/' component={LoginPage} exact={true} />
+        <PrivateRoute path='/Dashboard' component={EntryList} />
+        <PrivateRoute path='/Cupboard' component={CupboardItemList} />
+        <PrivateRoute path='/Create' component={CreateItem} />
+        <PrivateRoute path='/Edit/:id' component={EditItem} />
+        <PrivateRoute path='/Help' component={Help} />
+        <Route path='/About' component={Help} />
+        <Route path='/Login' component={LoginPage} />
+        <Route component={Error404} />
+      </Switch>
     </div>
-  </BrowserRouter>
+  </Router>
 );
 
 // auth router: presently nonfunctional
